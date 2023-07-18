997ce2abb349f9734b61852fdc59d4b974fc4e3c
syamozipc <syamozipc@gmail.com>
2023-07-19 00:17:31 +0900

    rdsとセキュリティグループを設定

--- production/local.tf
+++ production/local.tf
@@ -8,6 +8,9 @@ locals {
   client_domain_name = "web-app.${local.domain_name}"
   server_domain_name = "web-app-api.${local.domain_name}"
 
+  # TODO:vpcをterraform化したら動的に修正
+  vpc_id     = "vpc-0931b2de5ac2ea3a5"
+  subnet_ids = ["subnet-0c52ea815c78afeb2", "subnet-0b23ae07ff17c344f"]
 
   tags = {
     name      = local.name
