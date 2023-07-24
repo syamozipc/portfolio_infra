f7b69accb85432497e10aa151fc9dbdbadd2c39b
syamozipc <syamozipc@gmail.com>
2023-07-24 23:27:18 +0900

    alb周辺を作成

---
+++ production/alb.tf
@@ -0,0 +1,62 @@
+resource "aws_lb" "this" {
+  ip_address_type = "ipv4"
+  name            = "${local.name}-${local.env}"
+  security_groups = [aws_security_group.alb.id]
+  # TODO:動的にする
+  subnets = [for id in local.public_subnet_ids : id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    # host, path, queryはリクエスト値をそのまま使用
+    redirect {
+      status_code = "HTTP_301"
+      protocol    = "HTTPS"
+      port        = "443"
+    }
+  }
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  # TODO:動的にする
+  certificate_arn = var.acm_certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.id
+  }
+}
+
+resource "aws_lb_target_group" "this" {
+  name     = "${local.name}-${local.env}"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = local.vpc_id
+
+  health_check {
+    matcher = "200"
+    path    = "/api"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "first_ec2" {
+  target_group_arn = aws_lb_target_group.this.arn
+  target_id        = aws_instance.first.id
+  port             = 8080
+}
+
+resource "aws_lb_target_group_attachment" "second_ec2" {
+  target_group_arn = aws_lb_target_group.this.arn
+  target_id        = aws_instance.second.id
+  port             = 8080
+}
--- production/security_groups.tf
+++ production/security_groups.tf
@@ -26,15 +26,41 @@ resource "aws_security_group" "ec2" {
   vpc_id = local.vpc_id
 
   ingress {
-    from_port = 8080
-    to_port   = 8080
-    protocol  = "tcp"
-    # TODO:ALBをterraform化したら動的に修正
-    security_groups = ["sg-088cf4af5e5244a1c"]
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
   }
   ingress {
     from_port   = 22
     to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # 全て許可
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "${local.name}-${local.env}-alb-sg"
+  # TODO:vpcをterraform化したら動的に修正
+  vpc_id = local.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
